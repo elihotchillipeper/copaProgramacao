c9d8d4ce336503a046d548353098e300eedd8360
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-05-25 01:01:07 +0000

    copa programação

--- debugChallenge02.cpp
+++ debugChallenge02.cpp
@@ -7,8 +7,8 @@ int main() {
         cout << "O número é igual a 10." << std::endl;
     } else {
         std::cout << "O número não é igual a 10." << std::endl;}
-        cout << "Este é um código com erros" << endl; //sem ponto vírgula
-        cout << "Revise o código e aponte os erros" <<endl; //sem "<<" e sem ponto e vírgula
+        //cout << "Este é um código com erros" << endl; //sem ponto vírgula
+        //cout << "Revise o código e aponte os erros" <<endl; //sem "<<" e sem ponto e vírgula
         
         return 0;
 }
